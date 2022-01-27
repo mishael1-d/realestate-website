a6a9d35f3ff93129cb1d81b2e9395d036da41d45
Mishael <Mishaeldada@gmail.com>
2022-01-27 21:51:15 +0100

    refactoring

--- src/components/ContactPage/ContactPage.js
+++ src/components/ContactPage/ContactPage.js
@@ -30,55 +30,6 @@ function ContactPage() {
           <button className="primary">Contact Support</button>
         </div>
       </div>
-      {/* <div className="account-container container contact-container">
-        <div className="left-box">
-          <h2>Contact Info</h2>
-          <p>
-            To stay connected with us <br />
-            Please login with your personal info
-          </p>
-          <button>SIGN IN</button>
-        </div>
-        <div className="right-box">
-          <h2>Create Account</h2>
-          <p>
-            Please fill in the required input<sup>*</sup>
-          </p>
-          <form className="form-container">
-            <input
-              type="text"
-              name="name"
-              placeholder="Name"
-            //   onChange={onValueChange}
-              required
-            />
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-            //   onChange={onValueChange}
-              required
-            />
-            <input
-              type="password"
-              name="password"
-              placeholder="Password"
-            //   onChange={onValueChange}
-              required
-            />
-            <input
-              type="password"
-              name="confirmPassword"
-              placeholder="Confirm Password"
-            //   onChange={onValueChange}
-              required
-            />
-            <button type="submit">
-              SIGN UP
-            </button>
-          </form>
-        </div>
-      </div> */}
     </>
   );
 }
