441d14106d23f5d5906a496b8696bd05f7735f4f
Mishael <Mishaeldada@gmail.com>
2022-02-16 04:52:33 +0100

    Added slide animation to pages

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -1,54 +1,79 @@
 import React from "react";
+import Slide from "react-reveal/Slide";
 import "./Home.css";
 import About from "../AboutPage/About";
 import Video from "../VideoSection/Video";
-import Properties from "../Properties/Properties"
-import Contact from "../ContactPage/Contact"
-import Footer from "../Footer/Footer"
+import Properties from "../Properties/Properties";
+import Contact from "../ContactPage/Contact";
+import Footer from "../Footer/Footer";
 import TabNav from "./TabNav";
 
 function Home() {
   return (
     <>
-    <div className=" container">
-      <section className="hero-section">
-        <div className="text">
-          <h1>
-            Modern home <br />
-            Your <span>best</span> home
-          </h1>
-          <p>Let's find a perfect home for you</p>
-        </div>
-        <image className="image"></image>
-      </section>
-      <TabNav/>
-      <article className="article">
-        <div className="text">
-          For over 10 years we have been helping <br />
-          you find the best luxury property <br />at affordable prices.
-        </div>
-        <div className="figures">
-          <span className="fig-subtitle">
-          <p>10+</p> <br />
-              <span>Years <br />Of experience</span>
-          </span>
-          <span className="fig-subtitle">
-          <p>879</p> <br />
-              <span>Rented <br />Apartments</span>
-          </span>
-          <span className="fig-subtitle">
-          <p>20</p> <br />
-              <span>Awards <br />Gained</span>
-          </span>
-        </div>
-      </article>
-      <About/>
+      <div className=" container">
+        <section className="hero-section">
+          <Slide left cascade>
+            <div className="text">
+              <h1>
+                Modern home <br />
+                Your <span>best</span> home
+              </h1>
+              <p>Let's find a perfect home for you</p>
+            </div>
+          </Slide>
+          <Slide right>
+            <image className="image"></image>
+          </Slide>
+        </section>
+        <TabNav />
+        <Slide buttom>
+
+        <article className="article">
+          <div className="text">
+            For over 10 years we have been helping <br />
+            you find the best luxury property <br />
+            at affordable prices.
+          </div>
+          <div className="figures">
+            <span className="fig-subtitle">
+              <p>10+</p> <br />
+              <span>
+                Years <br />
+                Of experience
+              </span>
+            </span>
+            <span className="fig-subtitle">
+              <p>879</p> <br />
+              <span>
+                Rented <br />
+                Apartments
+              </span>
+            </span>
+            <span className="fig-subtitle">
+              <p>20</p> <br />
+              <span>
+                Awards <br />
+                Gained
+              </span>
+            </span>
+          </div>
+        </article>
+        </Slide>
+        <Slide left>
+          <About />
+        </Slide>
       </div>
-      <Video/>
-      <Properties />
-      <Contact />
+      <Slide right>
+        <Video />
+      </Slide>
+      <Slide left>
+        <Properties />
+      </Slide>
+      <Slide right>
+        <Contact />
+      </Slide>
       <Footer />
-      
     </>
   );
 }
