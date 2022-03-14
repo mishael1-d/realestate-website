a2c3144ea44dbc99f7fd96f6705e22cb52803bc8
Mishael <Mishaeldada@gmail.com>
2022-03-14 17:13:14 +0100

    video section redesigned

--- src/components/HomePage/Home.js
+++ src/components/HomePage/Home.js
@@ -28,37 +28,40 @@ function Home() {
         </section>
         <TabNav />
         <Slide buttom>
-
-        <article className="article">
-          <div className="text">
-            For over 10 years we have been helping <br />
-            you find the best luxury property <br />
-            at affordable prices.
-          </div>
-          <div className="figures">
-            <span className="fig-subtitle">
-              <p>10+</p> <br />
-              <span>
-                Years <br />
-                Of experience
+          <article className="article">
+            <div className="text">
+              <h5 className="text-title">Real . Estate</h5>
+              <h2 className="text-subtitle">Our Experience</h2>
+              <p className="text-body">
+                For over 10 years we have been helping <br />
+                you find the best luxury property <br />
+                at affordable prices.
+              </p>
+            </div>
+            <div className="figures">
+              <span className="fig-subtitle">
+                <p>10+</p> <br />
+                <span>
+                  Years <br />
+                  Of experience
+                </span>
               </span>
-            </span>
-            <span className="fig-subtitle">
-              <p>879</p> <br />
-              <span>
-                Rented <br />
-                Apartments
+              <span className="fig-subtitle">
+                <p>879</p> <br />
+                <span>
+                  Rented <br />
+                  Apartments
+                </span>
               </span>
-            </span>
-            <span className="fig-subtitle">
-              <p>20</p> <br />
-              <span>
-                Awards <br />
-                Gained
+              <span className="fig-subtitle">
+                <p>20</p> <br />
+                <span>
+                  Awards <br />
+                  Gained
+                </span>
               </span>
-            </span>
-          </div>
-        </article>
+            </div>
+          </article>
         </Slide>
         <Slide left>
           <About />
--- src/components/VideoSection/Video.js
+++ src/components/VideoSection/Video.js
@@ -2,6 +2,13 @@ import React from "react";
 import "./Video.css";
 import VideoPlayer from "react-video-js-player";
 import Src from "./video.mp4";
+import Src1 from "./video1.mp4";
+import Src2 from "./video2.mp4";
+import Src3 from "./video3.mp4";
+import poster from "../../images/unsplash_bfOQSDwEFg4.png";
+import poster1 from "../../images/unsplash_Id7u0EkTjBE.png";
+import poster2 from "../../images/unsplash_CLKGGwIBTaY.png";
+import poster3 from "../../images/unsplash_JvQ0Q5IkeMM.png";
 
 function Video() {
   const videoSrc = Src;
@@ -9,24 +16,60 @@ function Video() {
     <section className="video-section">
       <div className="container">
         <article className="heading">
-          <h3>Get Inspired</h3>
-          <article>
-            <p>
-              Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat
-              eligendi atque veritatis debitis, nihil officia molestias laborum
-              eaque. Voluptatibus culpa pariatur quas. Sit, sint neque. Velit
-              fuga fugiat necessitatibus et!
-            </p>
-            <button className="primary">View all</button>
-          </article>
+          <div className="text">
+            <h3>Get Inspired</h3>
+            <article>
+              <p>
+                We always give the best services to customers willing to buy
+                properties and investors willing to list out their properties.
+              </p>
+              <button className="primary">View all</button>
+            </article>
+          </div>
+          <div className="video">
+            <VideoPlayer
+              src={videoSrc}
+              width="500"
+              height="300"
+              controls={true}
+              poster={poster3}
+            />
+          </div>
         </article>
-        <div className="video">
-          <VideoPlayer
-            src={videoSrc}
-            width="710"
-            height="400"
-            controls={true}
-          />
+        <hr />
+        <div>
+          <article className="more-videos-heading">
+            <p>More Videos &gt;&gt;&gt;</p>
+            <div className="more-videos-video">
+              <div className="video" id="video-player-1647271082351">
+                <VideoPlayer
+                  src={Src1}
+                  width="500"
+                  height="300"
+                  controls={true}
+                  poster={poster1}
+                />
+              </div>
+              <div className="video">
+                <VideoPlayer
+                  src={Src2}
+                  width="500"
+                  height="300"
+                  controls={true}
+                  poster={poster2}
+                />
+              </div>
+              <div className="video">
+                <VideoPlayer
+                  src={Src3}
+                  width="500"
+                  height="300"
+                  controls={true}
+                  poster={poster}
+                />
+              </div>
+            </div>
+          </article>
         </div>
       </div>
     </section>
